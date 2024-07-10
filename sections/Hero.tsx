64a900f6dfdad7725f181b5778ae1b65f2a0ed65
Karla Oshikawa <karla_hara@hotmail.com>
2024-07-11 05:53:59 +0900

    feat: troca e devolucao

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,100 +1,35 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
-export interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  outline?: boolean;
+export interface HeroBlocks {
+  label: string;
+  image: ImageWidget;
 }
 
 export interface Props {
-  /**
-   * @format rich-text
-   * @default Click here to tweak this text however you want.
-   */
-  title?: string;
-  /**
-   * @default This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.
-   */
-  description?: string;
-  image?: ImageWidget;
-  placement?: "left" | "right";
-  cta?: CTA[];
+  imageCard: HeroBlocks[];
 }
 
-const PLACEMENT = {
-  left: "flex-col text-left lg:flex-row-reverse",
-  right: "flex-col text-left lg:flex-row",
-};
-
-export default function HeroFlats({
-  title = "Click here to tweak this text however you want.",
-  description =
-    "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
-  image,
-  placement = "left",
-  cta = [
-    { id: "change-me-1", href: "/", text: "Change me", outline: false },
-    { id: "change-me-2", href: "/", text: "Change me", outline: true },
-  ],
-}: Props) {
+export default function HeroFlats({ imageCard }: Props) {
   return (
-    <nav class="lg:container lg:mx-auto mx-4">
-      <div class="flex flex-col items-center gap-8">
+    <div class="md:w-[1300px] flex  justify-center items-center  h-[600px] mx-auto">
+      {imageCard.map((item, index) => (
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
-            image
-              ? PLACEMENT[placement]
-              : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          key={index}
+          class="box  w-full h-full sm:w-1/2 md:w-1/3 lg:w-1/4 relative group overflow-hidden rounded-md shadow-md transition-transform duration-500 ease-in-out hover:w-[1024px]"
         >
-          {image && (
-            <Image
-              width={640}
-              class="w-full lg:w-1/2 object-fit"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={image}
-              alt={image}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
-          <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
-            }`}
-          >
-            <div
-              class="inline-block lg:text-[80px] text-4xl leading-none font-medium"
-              dangerouslySetInnerHTML={{
-                __html: title,
-              }}
-            >
-            </div>
-            <p class="text-lg md:text-md leading-[150%]">
-              {description}
-            </p>
-            <div class="flex items-center gap-3">
-              {cta?.map((item) => (
-                <a
-                  key={item?.id}
-                  id={item?.id}
-                  href={item?.href}
-                  target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary ${
-                    item.outline && "btn-outline"
-                  }`}
-                >
-                  {item?.text}
-                </a>
-              ))}
-            </div>
-          </div>
+          <Image
+            width={1024}
+            height={500}
+            class="absolute inset-0  px-3 w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+            src={item.image}
+            alt={item.label}
+          />
+          <span class="absolute inset-0 flex items-center justify-center text-center text-white text-2xl font-bold  transition-opacity duration-500 ease-in-out group-hover:opacity-100 group-hover:text-5xl">
+            {item.label}
+          </span>
         </div>
-      </div>
-    </nav>
+      ))}
+    </div>
   );
 }
